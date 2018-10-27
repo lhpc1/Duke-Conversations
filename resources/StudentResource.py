42cfd03bce83a2c8f1eba1018883fb3ace0c869f
yasab27 <yasab27@gmail.com>
2018-10-26 23:24:33 -0400

    Fixed Cross Origin Error

--- resources/StudentResource.py
+++ resources/StudentResource.py
@@ -86,7 +86,7 @@ class StudentResource(Resource):
 
         studentOfInterest.save_to_db()
 
-        return StudentModel.find_by_id(uniqueID).json()
+        return StudentModel.find_by_id(uniqueID).json(), 500, {"Access-Control-Allow-Origin":"*"}
 
     def delete(self,netID):
 
